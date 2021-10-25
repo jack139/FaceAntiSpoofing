185dbbcaf715a87eda46ac45a6d5ffa3e954dc61
gt <jack139@gmail.com>
2021-10-25 14:47:14 +0800

    num_workers=0

--- CDCN-rPPG/train.py
+++ CDCN-rPPG/train.py
@@ -1,4 +1,6 @@
 import os
+import random
+import numpy as np
 import torch
 from torchvision import transforms, models
 from torch.utils.tensorboard import SummaryWriter
@@ -8,6 +10,14 @@ from utils.utils import read_cfg, get_optimizer, get_device, build_network
 from trainer.FASTrainer import FASTrainer
 from models.loss import DepthPPGLoss, DepthLoss
 from torch.optim.lr_scheduler import StepLR
+
+seed = 3
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+#torch.backends.cudnn.deterministic = True
+#torch.backends.cudnn.benchmark = False
+
 
 cfg = read_cfg(cfg_file="config/CDCN_adam_lr1e-3.yaml")
 #cfg = read_cfg(cfg_file="config/CDCNpp_adam_lr1e-3.yaml")
@@ -74,14 +84,14 @@ trainloader = torch.utils.data.DataLoader(
     dataset=trainset,
     batch_size=cfg['train']['batch_size'],
     shuffle=True,
-    num_workers=2
+    num_workers=0
 )
 
 valloader = torch.utils.data.DataLoader(
     dataset=valset,
     batch_size=cfg['val']['batch_size'],
     shuffle=True,
-    num_workers=2
+    num_workers=0
 )
 
 trainer = FASTrainer(
